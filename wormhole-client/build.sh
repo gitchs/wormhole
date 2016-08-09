a5c7b65c47c3141fe6f59a0e60b2f7534a7d3d45
tinyproxy <tinyproxy@gmail.com>
2016-08-09 18:01:39 +0800

    fix bash syntax error

--- wormhole-client/build.sh
+++ wormhole-client/build.sh
@@ -2,8 +2,8 @@
 OSES=(linux darwin windows freebsd)
 ARCHS=(amd64 386)
 COMMIT=$(git log|head -n 1|awk '{print $2}')
-for OS in ${OSES};do
-    for ARCH in ${ARCHS};do
+for OS in ${OSES[@]};do
+    for ARCH in ${ARCHS[@]};do
         filename="wormhole-client-${OS}-${ARCH}-${COMMIT}"
         GOOS="${OS}" GOARCH="${ARCH}" go build -o "$filename" ./
         xz "${filename}"
